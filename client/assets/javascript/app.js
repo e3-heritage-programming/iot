055bdb640af703e8832532c94be66660dd39cdb4
CraftThatBlock <craftthatblock@gmail.com>
2015-05-20 15:25:54 -0400

    Fix client jQuery

--- client/assets/javascript/app.js
+++ client/assets/javascript/app.js
@@ -1,3 +1,4 @@
+var jQuery = require('../../bower_components/jquery/jquery');
 var Script = require('./script');
 var Bootstrap = require('../../bower_components/bootstrap-sass/assets/javascripts/bootstrap');
 
